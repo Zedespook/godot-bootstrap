3b10b43870503022054e399b7af9f132c2639d43
Zedespook <4380045-Zedespook@users.noreply.gitlab.com>
2022-01-24 11:22:44 +0100

    Changed `setup.py` because only it's top-down atm

--- godot/setup.py
+++ godot/setup.py
@@ -8,20 +8,20 @@ def main():
         print("Please provide an argument!")
         print("  Arguments:")
         print("    --2d-topdown")
-        print("    --2d-side")
-        print("    --2d-topdown-multiplayer")
-        print("    --2d-side-multiplayer")
+        # print("    --2d-side")
+        # print("    --2d-topdown-multiplayer")
+        # print("    --2d-side-multiplayer")
         return
 
     # Checkout to the branch provided within the arugment
     if sys.argv[1] == "--2d-topdown":
         os.system("git checkout 2d-topdown")
-    elif sys.argv[1] == "--2d-side":
-        os.system("git checkout 2d-side")
-    elif sys.argv[1] == "--2d-topdown-multiplayer":
-        os.system("git checkout 2d-topdown-multiplayer")
-    elif sys.argv[1] == "--2d-side-multiplayer":
-        os.system("git checkout 2d-side-multiplayer")
+    # elif sys.argv[1] == "--2d-side":
+    #     os.system("git checkout 2d-side")
+    # elif sys.argv[1] == "--2d-topdown-multiplayer":
+    #     os.system("git checkout 2d-topdown-multiplayer")
+    # elif sys.argv[1] == "--2d-side-multiplayer":
+    #     os.system("git checkout 2d-side-multiplayer")
 
 
 if __name__ == "__main__":
